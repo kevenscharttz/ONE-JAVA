e8f665428e360c6a17e44dd88758f087cf8b6765
Keven Willians <149530293+kevenscharttz@users.noreply.github.com>
2025-04-04 01:22:40 -0300

    Add files via upload

---
+++ DesafioJava1/src/Estudos.java
@@ -0,0 +1,5 @@
+public class Estudos {
+    public static void main(String[] args) {
+        System.out.println("Essas são minhas anotações de Java: Estou gostando até o momento :)");
+    }
+}
---
+++ PrimeiraClasse/src/PrimeiraClasse.java
@@ -0,0 +1,5 @@
+public class PrimeiraClasse {
+    public static void main(String[] args){
+        System.out.println("Concluí a aula 1 e agora estou mergulhando em Java!");
+    }
+}
