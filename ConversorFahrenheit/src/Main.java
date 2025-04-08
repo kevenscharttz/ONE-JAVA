a0d70e65cb5019e09dbbd09600483ab1c89ff959
Keven Willians <149530293+kevenscharttz@users.noreply.github.com>
2025-04-08 01:27:12 -0300

    Add files via upload

---
+++ ConversorFahrenheit/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        double celcius = 32.68;
+
+        int fahrenheit = (int) (celcius * 1.8) + 32;
+
+        System.out.println("A temperatura atual está em " +celcius+" graus Celcius, é o equivalente a " +fahrenheit+" fahrenheit");
+
+    }
+}
